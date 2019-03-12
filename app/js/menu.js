cd219d91baa2b866ee851c8471ed75c1a1169beb
Nataliaiv <karamelshokoladovna@gmail.com>
2019-03-13 00:08:47 +0700

    Ally Tab

--- app/js/menu.js
+++ app/js/menu.js
@@ -4,37 +4,70 @@
 
   var mainNav = document.querySelector('.main-nav');
   var buns = document.querySelector('.main-nav__buttons');
-  var toggle = document.querySelector('.main-nav__toggle');
+  var toggle = document.querySelector('.main-nav__toggle-inner');
 
   mainNav.classList.remove('main-nav--no-js');
 
-  buns.addEventListener('click', function(evt) {
-    evt.preventDefault();
+  function toggleMenu() {
+    buns.addEventListener('click', function (evt) {
+      evt.preventDefault();
 
-    if(mainNav.classList.contains('main-nav--closed')) {
-      mainNav.classList.remove('main-nav--closed');
-      mainNav.classList.add('main-nav--opened');
-      toggle.classList.add('main-nav__toggle--show');
-      $('.main-nav__list').slideDown();
-    } else {
-      mainNav.classList.add('main-nav--closed');
-      mainNav.classList.remove('main-nav--opened');
-      toggle.classList.remove('main-nav__toggle--show');
-      $('.main-nav__list').slideUp();
-    }
-  });
+      if (mainNav.classList.contains('main-nav--closed')) {
+        mainNav.classList.remove('main-nav--closed');
+        mainNav.classList.add('main-nav--opened');
+        toggle.classList.add('main-nav__toggle--show');
+        $('.main-nav__list').slideDown();
+      } else {
+        mainNav.classList.add('main-nav--closed');
+        mainNav.classList.remove('main-nav--opened');
+        toggle.classList.remove('main-nav__toggle--show');
+        $('.main-nav__list').slideUp();
+      }
+    });
 
-  $(document).ready(function() {
+    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    var focusableElements = mainNav.querySelectorAll(focusableElementsString);
+    focusableElements = Array.prototype.slice.call(focusableElements);
+    var firstTabStop = focusableElements[0];
+    var lastTabStop = focusableElements[focusableElements.length - 1];
+    firstTabStop.focus();
+    mainNav.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 9) {
+        if (evt.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+            evt.preventDefault();
+            lastTabStop.focus();
+          }
+        } else {
+          if (document.activeElement === lastTabStop) {
+            evt.preventDefault();
+            firstTabStop.focus();
+          }
+        }
+      }
+    });
+    
+    mainNav.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        mainNav.classList.add('main-nav--closed');
+        mainNav.classList.remove('main-nav--opened');
+        toggle.classList.remove('main-nav__toggle--show');
+      }
+    });
+  }
+
+  $(document).ready(function () {
     var headerHeight = $('.header').outerHeight();
-    $('.main-nav__link').click(function(evt) {
+    $('.main-nav__link').click(function (evt) {
       evt.preventDefault();
       var linkHref = $(this).attr('href');
       $('html, body').animate({
-        scrollTop: $(linkHref).offset().top - HEADER_HEIGHT_COOFICIENT*headerHeight
+        scrollTop: $(linkHref).offset().top - HEADER_HEIGHT_COOFICIENT * headerHeight
       }, 1000);
       $('.main-nav__list').slideUp();
       toggle.classList.remove('main-nav__toggle--show');
       mainNav.classList.add('main-nav--closed');
     })
-  })
+  });
+  toggleMenu();
 })()
--- app/js/popup.js
+++ app/js/popup.js
@@ -1,23 +1,59 @@
 'use strict';
 (function () {
-var modal = document.querySelector('#popup-sign-up');
-var modalClose = modal.querySelector('.popup-sign-up__close');
+    var modal = document.querySelector('#popup-sign-up');
+    var modalClose = modal.querySelector('.popup-sign-up__close');
+    var lastFocusedElement;
 
-function showPopup() {
-    $('.schedule-and-cost__sign-up').on('click', function (evt) {
-        evt.preventDefault;
-        modal.style.display = 'block';
-    })
-}
+    function showPopup() {
+        closePopup();
+        lastFocusedElement = $('.schedule-and-cost__sign-up');
 
-function closePopup() {
-    window.addEventListener('click', function (evt) {
-        evt.preventDefault;
-        if (evt.target == modal || event.target == modalClose) {
-            modal.style.display = "none";
-        }
-    })
-}
-showPopup();
-closePopup();
+        lastFocusedElement.on('click', function (evt) {
+            evt.preventDefault;
+            modal.style.display = 'block';
+        })
+        var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+        var focusableElements = modal.querySelectorAll(focusableElementsString);
+        focusableElements = Array.prototype.slice.call(focusableElements);
+        var firstTabStop = focusableElements[0];
+        var lastTabStop = focusableElements[focusableElements.length - 1];
+        firstTabStop.focus();
+
+        modal.addEventListener('keydown', function (evt) {
+            if (evt.keyCode === 9) {
+                if (evt.shiftKey) {
+                    if (document.activeElement === firstTabStop) {
+                        evt.preventDefault();
+                        lastTabStop.focus();
+                    }
+                } else {
+                    if (document.activeElement === lastTabStop) {
+                        evt.preventDefault();
+                        firstTabStop.focus();
+                    }
+                }
+            }
+        });
+    }
+
+    function closePopup() {
+        modal.addEventListener('click', function (evt) {
+            evt.preventDefault;
+            if (evt.target == modal || event.target == modalClose) {
+                modal.style.display = "none";
+                lastFocusedElement.focus();
+            }
+        });
+
+        modal.addEventListener('keydown', function (evt) {
+            if (evt.keyCode === 27) {
+                modal.style.display = "none";
+                lastFocusedElement.focus();
+            }
+        });
+
+    }
+
+    showPopup();
+    closePopup();
 })();
